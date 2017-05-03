03207039e5910e1986d367302696d623c063c4c6
Dan Fitch <dgfitch@google.com>
2017-05-03 01:22:03 -0500

    Duh, C

--- audio.c
+++ audio.c
@@ -1,28 +1,34 @@
 #include <stdio.h>
+#include <string.h>
 #include <assert.h>
 #include <pulse/pulseaudio.h>
 
 // TODO - yet to port
 #define BUFSIZE 1024
-#define SOURCE_NAME = "cras-source" // edit to match your source
-#define METER_RATE = 60
-#define MAX_SAMPLE_VALUE = 127
-#define DEBUG = 0
-#define SMOOTH = 0
+#define SOURCE_NAME "cras-source" // edit to match your source
+#define METER_RATE 60
+#define MAX_SAMPLE_VALUE 127
+#define DEBUG 0
+#define SMOOTH 0
 
 #define FORMAT PA_SAMPLE_U8
 #define RATE 44100
 
 void context_state_cb(pa_context* context, void* mainloop);
-void stream_state_cb(pa_stream *s, void *mainloop);
-void stream_success_cb(pa_stream *stream, int success, void *userdata);
-void stream_write_cb(pa_stream *stream, size_t requested_bytes, void *userdata);
+void source_info_cb(pa_context *context, const pa_source_info *i, int eol, void *userdata);
+void stream_read_callback(pa_stream *s, size_t length, void *userdata);
+
+typedef struct pa_devicelist {
+        uint8_t initialized;
+        char name[512];
+        uint32_t index;
+        char description[256];
+} pa_devicelist_t;
 
 int main(int argc, char *argv[]) {
     pa_threaded_mainloop *mainloop;
     pa_mainloop_api *mainloop_api;
     pa_context *context;
-    pa_stream *stream;
 
     // Get a mainloop and its context
     mainloop = pa_threaded_mainloop_new();
@@ -41,91 +47,63 @@ int main(int argc, char *argv[]) {
     assert(pa_threaded_mainloop_start(mainloop) == 0);
     assert(pa_context_connect(context, NULL, PA_CONTEXT_NOAUTOSPAWN, NULL) == 0);
 
+		printf("Started mainloop\n");
+
     // Wait for the context to be ready
-    for(;;) {
+    while(1) {
+				printf("Checking context state\n");
         pa_context_state_t context_state = pa_context_get_state(context);
         assert(PA_CONTEXT_IS_GOOD(context_state));
         if (context_state == PA_CONTEXT_READY) break;
         pa_threaded_mainloop_wait(mainloop);
     }
 
-    // Create a playback stream
-    pa_sample_spec sample_specifications;
-    sample_specifications.format = FORMAT;
-    sample_specifications.rate = RATE;
-    sample_specifications.channels = 2;
+		printf("Context ready\n");
+		printf("Running op\n");
 
-    pa_channel_map map;
-    pa_channel_map_init_stereo(&map);
+		pa_operation *op;
+		pa_devicelist_t input[16];
+	  memset(input, 0, sizeof(pa_devicelist_t) * 16);
+		op = pa_context_get_source_info_list(context, &source_info_cb, input);
+		pa_operation_unref(op);
+		printf("Waiting\n");
 
-    stream = pa_stream_new(context, "Playback", &sample_specifications, &map);
-    pa_stream_set_state_callback(stream, stream_state_cb, mainloop);
-    pa_stream_set_write_callback(stream, stream_write_cb, mainloop);
-
-    // recommended settings, i.e. server uses sensible values
-    pa_buffer_attr buffer_attr; 
-    buffer_attr.maxlength = (uint32_t) -1;
-    buffer_attr.tlength = (uint32_t) -1;
-    buffer_attr.prebuf = (uint32_t) -1;
-    buffer_attr.minreq = (uint32_t) -1;
-
-    // Settings copied as per the chromium browser source
-    pa_stream_flags_t stream_flags;
-    stream_flags = PA_STREAM_START_CORKED | PA_STREAM_INTERPOLATE_TIMING | 
-        PA_STREAM_NOT_MONOTONIC | PA_STREAM_AUTO_TIMING_UPDATE |
-        PA_STREAM_ADJUST_LATENCY;
-
-    // Connect stream to the default audio output sink
-    assert(pa_stream_connect_playback(stream, NULL, &buffer_attr, stream_flags, NULL, NULL) == 0);
-
-    // Wait for the stream to be ready
-    for(;;) {
-        pa_stream_state_t stream_state = pa_stream_get_state(stream);
-        assert(PA_STREAM_IS_GOOD(stream_state));
-        if (stream_state == PA_STREAM_READY) break;
-        pa_threaded_mainloop_wait(mainloop);
-    }
-
-    pa_threaded_mainloop_unlock(mainloop);
-
-    // Uncork the stream so it will start playing
-    pa_stream_cork(stream, 0, stream_success_cb, mainloop);
-
-    // Play until we get a character
+    // Go until we get a character
     getc(stdin);
+		return(0);
 }
 
 void context_state_cb(pa_context* context, void* mainloop) {
     pa_threaded_mainloop_signal(mainloop, 0);
 }
 
-void stream_state_cb(pa_stream *s, void *mainloop) {
-    pa_threaded_mainloop_signal(mainloop, 0);
+void source_info_cb(pa_context *context, const pa_source_info *i, int eol, void *userdata) {
+		if (i->name == SOURCE_NAME) {
+				static const pa_sample_spec ss = {
+						.format = PA_SAMPLE_U8,
+						.rate = RATE,
+						.channels = 1
+				};
+				pa_stream *stream = NULL;
+				stream = pa_stream_new(context, "peak detect demo", &ss, NULL);
+			  const int *index = &(i->index);
+			  pa_stream_set_read_callback(stream, &stream_read_callback, &index);
+				pa_stream_connect_record(stream, NULL, NULL, PA_STREAM_PEAK_DETECT);
+		}
 }
 
-void stream_write_cb(pa_stream *stream, size_t requested_bytes, void *userdata) {
-    int bytes_remaining = requested_bytes;
-    while (bytes_remaining > 0) {
-        uint8_t *buffer = NULL;
-        size_t bytes_to_fill = 44100;
-        size_t i;
+void stream_read_callback(pa_stream *s, size_t length, void *userdata) {
+    const void *data;
+    assert(s);
+    assert(length > 0);
+		pa_stream_peek(s, &data, &length);
 
-        if (bytes_to_fill > bytes_remaining) bytes_to_fill = bytes_remaining;
+    assert(s);
+    assert(length > 0);
 
-        pa_stream_begin_write(stream, (void**) &buffer, &bytes_to_fill);
+		// TODO - do something with data
+		printf("Data is %p", data);
 
-        for (i = 0; i < bytes_to_fill; i += 2) {
-            buffer[i] = (i%100) * 40 / 100 + 44;
-            buffer[i+1] = (i%100) * 40 / 100 + 44;
-        }
-
-        pa_stream_write(stream, buffer, bytes_to_fill, NULL, 0LL, PA_SEEK_RELATIVE);
-
-        bytes_remaining -= bytes_to_fill;
-    }
-}
-
-void stream_success_cb(pa_stream *stream, int success, void *userdata) {
-    return;
+		pa_stream_drop(s);
 }
 
